17edda33c503aeca89951424b7349e490a55b2bd
Myth-Sun <1991773694@qq.com>
2020-11-09 19:54:06 +0800

    N皇后问题

--- src/NQueens.java
+++ src/NQueens.java
@@ -1,7 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
 
-//N皇后问题
+/**
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 给定一个整数 n，返回所有不同的n皇后问题的解决方案。
+ *
+ * 每一种解法包含一个明确的n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ * 示例：
+ *
+ * 输入：4
+ * 输出：[
+ *  [".Q..",  // 解法 1
+ *   "...Q",
+ *   "Q...",
+ *   "..Q."],
+ *
+ *  ["..Q.",  // 解法 2
+ *   "Q...",
+ *   "...Q",
+ *   ".Q.."]
+ * ]
+ * 解释: 4 皇后问题存在两个不同的解法。
+ * 提示：
+ * 皇后彼此不能相互攻击，也就是说：任何两个皇后都不能处于同一条横行、纵行或斜线上。
+ */
 public class NQueens {
 
     public List<List<String>> solveNQueens(int n) {
@@ -63,11 +85,5 @@ public class NQueens {
                 return false;
         }
         return true;
-    }
-
-    public static void main(String[] args) {
-        NQueens nQueens = new NQueens();
-        List<List<String>> lists = nQueens.solveNQueens(4);
-//        System.out.println(resList.toString());
     }
 }
