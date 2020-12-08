63ccc40565154c3800aea33cca9749625e92b92f
Myth-Sun <1991773694@qq.com>
2020-12-08 13:10:56 +0800

    动态规划：122. 买卖股票的最佳时机 II（不限制交易次数）

--- src/动态规划/其他经典问题/MaxProfit.java
+++ src/动态规划/其他经典问题/MaxProfit.java
@@ -71,28 +71,26 @@ public class MaxProfit {
         return dp[N - 1][K][0];
     }
 
-    /** 
+    /**
      * 买卖股票的最佳时机（简单）
      * 给定一个数组，它的第i 个元素是一支给定股票第 i 天的价格。
-     *
+     * <p>
      * 如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
-     *
+     * <p>
      * 注意：你不能在买入股票前卖出股票。
-     *
-     * 
-     *
+     * <p>
      * 示例 1:
-     *
+     * <p>
      * 输入: [7,1,5,3,6,4]
      * 输出: 5
      * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
-     *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+     * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
      * 示例 2:
-     *
+     * <p>
      * 输入: [7,6,4,3,1]
      * 输出: 0
      * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
-     *
+     * <p>
      * 来源：力扣（LeetCode）
      * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -114,12 +112,64 @@ public class MaxProfit {
         return dpI0;
     }
 
+    /**
+     * 买卖股票的最佳时机 II
+     * 给定一个数组，它的第i 个元素是一支给定股票第 i 天的价格。
+     * <p>
+     * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+     * <p>
+     * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: [7,1,5,3,6,4]
+     * 输出: 7
+     * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+     * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+     * 示例 2:
+     * <p>
+     * 输入: [1,2,3,4,5]
+     * 输出: 4
+     * 解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+     * 注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+     * 因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+     * 示例3:
+     * <p>
+     * 输入: [7,6,4,3,1]
+     * 输出: 0
+     * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+     * <p>
+     * 提示：
+     * <p>
+     * 1 <= prices.length <= 3 * 10 ^ 4
+     * 0 <= prices[i]<= 10 ^ 4
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int maxProfitKInf(int[] prices) {
+        //尽可能地完成交易-->K为正无穷，此时可以认为K和K-1是一样的
+        int N = prices.length;
+        //没有股票，返回0
+        if (N == 0)
+            return 0;
+
+        int dpI0 = 0, dpI1 = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            int temp = dpI0;
+            dpI0 = Math.max(dpI0, dpI1 + prices[i]);
+            dpI1 = Math.max(dpI1, temp - prices[i]);
+        }
+        return dpI0;
+    }
+
     public static void main(String[] args) {
         MaxProfit maxProfit = new MaxProfit();
         int k = 2;
         int[] prices = {7,6,4,3,1};
 //        int i = maxProfit.maxProfit(k, prices);
-        int i = maxProfit.maxProfitK1(prices);
+        int i = maxProfit.maxProfitKInf(prices);
         System.out.println(i);
 
     }
