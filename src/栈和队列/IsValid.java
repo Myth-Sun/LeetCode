4fffe27f9069533ab87f16d74cbdb843e76e8776
Myth-Sun <1991773694@qq.com>
2021-01-28 16:21:40 +0800

    20. 有效的括号

---
+++ src/栈和队列/IsValid.java
@@ -0,0 +1,75 @@
+package 栈和队列;
+
+import java.util.Stack;
+
+/**
+ * 20. 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "()"
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * 示例 3：
+ * <p>
+ * 输入：s = "(]"
+ * 输出：false
+ * 示例 4：
+ * <p>
+ * 输入：s = "([)]"
+ * 输出：false
+ * 示例 5：
+ * <p>
+ * 输入：s = "{[]}"
+ * 输出：true
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 104
+ * s 仅由括号 '()[]{}' 组成
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IsValid {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.empty() || stack.peek() != c) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        IsValid isValid = new IsValid();
+        String s = "((([])";
+        boolean valid = isValid.isValid(s);
+        System.out.println(valid);
+    }
+}
