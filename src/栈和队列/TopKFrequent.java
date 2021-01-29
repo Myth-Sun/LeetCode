273e8e3ac36997b74e6943805d175443f05d9a55
Myth-Sun <1991773694@qq.com>
2021-01-29 23:19:32 +0800

    347. 前 K 个高频元素(利用PriorityQueue)

---
+++ src/栈和队列/TopKFrequent.java
@@ -0,0 +1,77 @@
+package 栈和队列;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * 347. 前 K 个高频元素
+ * 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ * 示例 2:
+ * <p>
+ * 输入: nums = [1], k = 1
+ * 输出: [1]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+ * 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+ * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/top-k-frequent-elements
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+//利用PriorityQueue
+public class TopKFrequent {
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] ans = new int[k];
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        //int[]的第一个元素代表数组的值，第二个元素代表该值出现的次数
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            ans[i] = queue.poll()[0];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        TopKFrequent topKFrequent = new TopKFrequent();
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        int[] ints = topKFrequent.topKFrequent(nums, k);
+        for (int num : ints) {
+            System.out.println(num);
+        }
+    }
+}
