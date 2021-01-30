346f05836f5c9db5bb058a1c3669ea9e4e3522c3
Myth-Sun <1991773694@qq.com>
2021-01-30 20:22:37 +0800

    102. 二叉树的层序遍历

---
+++ src/树/LevelOrder.java
@@ -0,0 +1,71 @@
+package 树;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层序遍历
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * 二叉树：[3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其层序遍历结果：
+ * <p>
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+//利用队列
+public class LevelOrder {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                list.add(poll.val);
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
