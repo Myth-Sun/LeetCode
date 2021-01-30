73402f1b81909936e56570b7569f09f389adceef
Myth-Sun <1991773694@qq.com>
2021-01-30 21:26:55 +0800

    429. N 叉树的层序遍历

---
+++ src/树/LevelOrderII.java
@@ -0,0 +1,66 @@
+package 树;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 429. N 叉树的层序遍历
+ * 给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）。
+ *
+ * 树的序列化输入是用层序遍历，每组子节点都由 null 值分隔（参见示例）。
+ *
+ *  
+ *
+ * 示例 1：
+ * 输入：root = [1,null,3,2,4,null,5,6]
+ * 输出：[[1],[3,2,4],[5,6]]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class LevelOrderII {
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node poll = queue.poll();
+                list.add(poll.val);
+
+                for (Node node : poll.children) {
+                    if (node != null)
+                        queue.offer(node);
+                }
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+}
