51370fa2673f836bfce370fbdbe76a9dc05ada5d
Myth-Sun <1991773694@qq.com>
2021-02-01 17:56:15 +0800

    113. 路径总和 II

---
+++ src/树/PathSum.java
@@ -0,0 +1,77 @@
+package 树;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 113. 路径总和 II
+ * 给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ * <p>
+ * 示例:
+ * 给定如下二叉树，以及目标和 sum = 22，
+ * <p>
+ * 5
+ * / \
+ * 4   8
+ * /   / \
+ * 11  13  4
+ * /  \    / \
+ * 7    2  5   1
+ * 返回:
+ * <p>
+ * [
+ * [5,4,11,2],
+ * [5,8,4,5]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/path-sum-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class PathSum {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<List<Integer>> ret = new LinkedList<List<Integer>>();
+    Deque<Integer> path = new LinkedList<Integer>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        dfs(root, sum);
+        return ret;
+    }
+
+    public void dfs(TreeNode root, int sum) {
+        if (root == null) {
+            return;
+        }
+        path.offerLast(root.val);
+        sum -= root.val;
+        if (root.left == null && root.right == null && sum == 0) {
+            ret.add(new LinkedList<Integer>(path));
+        }
+        dfs(root.left, sum);
+        dfs(root.right, sum);
+        path.pollLast();//最后回溯
+    }
+
+}
