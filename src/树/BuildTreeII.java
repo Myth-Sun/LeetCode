acca98723bd73e5c72d823b5a414c4dd440ed375
Myth-Sun <1991773694@qq.com>
2021-02-01 21:44:11 +0800

    105. 从前序与中序遍历序列构造二叉树

---
+++ src/树/BuildTreeII.java
@@ -0,0 +1,79 @@
+package 树;
+
+import java.util.HashMap;
+
+/**
+ * 105. 从前序与中序遍历序列构造二叉树
+ * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ * <p>
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ * <p>
+ * 例如，给出
+ * <p>
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 返回如下的二叉树：
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class BuildTreeII {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int[] preorder;//前序
+    int[] inorder;//中序
+    int preIndex = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        this.inorder = inorder;
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return helper(0, inorder.length - 1);
+    }
+
+    public TreeNode helper(int leftIndex, int rightIndex) {
+        if (leftIndex > rightIndex) {
+            return null;
+        }
+
+        int rootVal = preorder[preIndex];
+        int index = map.get(rootVal);
+        TreeNode root = new TreeNode(rootVal);
+        preIndex++;
+
+        //先生成左子树
+        root.left = helper(leftIndex, index - 1);
+        //再生成右子树
+        root.right = helper(index + 1, rightIndex);
+
+        return root;
+    }
+}
