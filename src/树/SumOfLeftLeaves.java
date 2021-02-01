7a85c214b54b60777d7fcc7f3894acbfc18595aa
Myth-Sun <1991773694@qq.com>
2021-02-01 14:02:44 +0800

    404. 左叶子之和

---
+++ src/树/SumOfLeftLeaves.java
@@ -0,0 +1,53 @@
+package 树;
+
+/**
+ * 404. 左叶子之和
+ * 计算给定二叉树的所有左叶子之和。
+ *
+ * 示例：
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-of-left-leaves
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+//无法判断当前节点是不是左节点，需要根据当前节点的父节点判断
+public class SumOfLeftLeaves {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    int sum = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root);
+        return sum;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+        dfs(root.left);
+        dfs(root.right);
+    }
+}
