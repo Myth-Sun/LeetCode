9a6b062a3c7b9921d53f0b4ddf669debea894b75
Myth-Sun <1991773694@qq.com>
2021-02-13 23:35:02 +0800

    135. 分发糖果

---
+++ src/贪心/Candy.java
@@ -0,0 +1,62 @@
+package 贪心;
+
+/*
+135. 分发糖果
+老师想给孩子们分发糖果，有 N个孩子站成了一条直线，老师会根据每个孩子的表现，预先给他们评分。
+
+你需要按照以下要求，帮助老师给这些孩子分发糖果：
+
+每个孩子至少分配到 1 个糖果。
+评分更高的孩子必须比他两侧的邻位孩子获得更多的糖果。
+那么这样下来，老师至少需要准备多少颗糖果呢？
+
+
+示例1：
+
+输入：[1,0,2]
+输出：5
+解释：你可以分别给这三个孩子分发 2、1、2 颗糖果。
+示例2：
+
+输入：[1,2,2]
+输出：4
+解释：你可以分别给这三个孩子分发 1、2、1 颗糖果。
+     第三个孩子只得到 1 颗糖果，这已满足上述两个条件。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/candy
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Candy {
+    //从左到右遍历，比较i比i-1大的情况
+    //从右到左遍历，比较i比i+1大的情况
+    public int candy(int[] ratings) {
+
+        int n = ratings.length;
+        int[] left = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
+            }
+        }
+        int ans = 0, right = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1 && ratings[i] > ratings[i + 1]) {
+                right++;
+            } else {
+                right = 1;
+            }
+            ans += Math.max(left[i], right);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Candy candy = new Candy();
+        int[] ratings = {1, 2, 2};
+        System.out.println(candy.candy(ratings));
+    }
+}
