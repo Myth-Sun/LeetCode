404b542c4e19c858982bba281f3d3ea96f47d6c0
Myth-Sun <1991773694@qq.com>
2021-02-19 15:04:13 +0800

    斐波那契数列

---
+++ src/数组/Fibonacci.java
@@ -0,0 +1,34 @@
+package 数组;
+
+/*
+斐波那契数列
+题目描述
+大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0，第1项是1）。
+n\leq 39n≤39
+
+示例1
+输入
+
+4
+返回值
+
+3
+ */
+public class Fibonacci {
+    public int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Fibonacci f = new Fibonacci();
+        System.out.println(f.fibonacci(4));
+    }
+}
