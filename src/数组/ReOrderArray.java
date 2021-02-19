444d5e6556528e3f9d0dcb403624367cd71e5ba9
Myth-Sun <1991773694@qq.com>
2021-02-19 16:08:29 +0800

    调整数组顺序使奇数位于偶数前面

---
+++ src/数组/ReOrderArray.java
@@ -0,0 +1,36 @@
+package 数组;
+
+/*
+	调整数组顺序使奇数位于偶数前面
+题目描述
+输入一个整数数组，实现一个函数来调整该数组中数字的顺序，
+使得所有的奇数位于数组的前半部分，所有的偶数位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+ */
+public class ReOrderArray {
+    public void reOrderArray(int[] array) {
+        int begin = 0, count = 0;
+        int[] newArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1)
+                count++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]%2==1)
+                newArray[begin++] = array[i];
+            else
+                newArray[count++] = array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            array[i] = newArray[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        ReOrderArray r = new ReOrderArray();
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        r.reOrderArray(array);
+        for (int num : array) {
+            System.out.printf(" " + num);
+        }
+    }
+}
