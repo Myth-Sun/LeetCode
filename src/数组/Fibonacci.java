f992a693ce438dc545bf19e51098209acd17eb9d
Myth-Sun <1991773694@qq.com>
2021-02-19 16:34:46 +0800

    改进斐波那契数列

--- src/数组/Fibonacci.java
+++ src/数组/Fibonacci.java
@@ -19,12 +19,13 @@ public class Fibonacci {
         if (n <= 1) {
             return n;
         }
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
+        int dp0 = 0, dp1 = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            int sum = (dp0 + dp1) % 1000000007;
+            dp0 = dp1;
+            dp1 = sum;
         }
-        return dp[n];
+        return dp1;
     }
 
     public static void main(String[] args) {
