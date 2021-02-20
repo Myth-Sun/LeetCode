1b401a3e90dc13fdfbb9a0dd45685af3d7bcc476
Myth-Sun <1991773694@qq.com>
2021-02-20 17:15:10 +0800

    剑指 Offer 40. 最小的k个数

---
+++ src/数组/GetLeastNumbers.java
@@ -0,0 +1,56 @@
+package 数组;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/*
+剑指 Offer 40. 最小的k个数
+输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+
+示例 1：
+
+输入：arr = [3,2,1], k = 2
+输出：[1,2] 或者 [2,1]
+示例 2：
+
+输入：arr = [0,1,2,1], k = 1
+输出：[0]
+   
+
+限制：
+
+0 <= k <= arr.length <= 10000
+0 <= arr[i]   <= 10000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class GetLeastNumbers {
+    //利用大根堆 PriorityQueue
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k == 0) {
+            return new int[]{};
+        }
+        int[] res = new int[k];
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        for (int i = 0; i < k; i++) {
+            queue.add(arr[i]);
+        }
+        for (int i = k; i < arr.length; i++) {
+            if (arr[i] < queue.peek()) {
+                queue.poll();
+                queue.add(arr[i]);
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            res[i] = queue.poll();
+        }
+        return res;
+    }
+}
