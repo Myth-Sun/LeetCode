05d9e0fae85e153649b8b1728019acf95586555c
Myth-Sun <1991773694@qq.com>
2021-02-22 21:53:22 +0800

    155. 最小栈

---
+++ src/栈和队列/MinStack.java
@@ -0,0 +1,80 @@
+package 栈和队列;
+
+import java.util.Stack;
+
+/*
+155. 最小栈
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) —— 将元素 x 推入栈中。
+pop() —— 删除栈顶的元素。
+top() —— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+
+示例:
+
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+ 
+
+提示：
+
+pop、top 和 getMin 操作总是在 非空栈 上调用。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/min-stack
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+//栈中的每一个元素声明为容量为2的数组，数组的两个元素分别为当前值和当前栈的最小值
+public class MinStack {
+    /**
+     * initialize your data structure here.
+     */
+    Stack<int[]> stack;
+
+    public MinStack() {
+        stack = new Stack<int[]>();
+    }
+
+    public void push(int x) {
+        if (stack.empty()) {
+            stack.push(new int[]{x, x});
+        } else {
+            stack.push(new int[]{x, Math.min(x, stack.peek()[1])});
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
