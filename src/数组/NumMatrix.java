9523bc7c30abb88f90300e18e3ca1cc7bf13fe79
Myth-Sun <1991773694@qq.com>
2021-03-02 14:21:26 +0800

    304. 二维区域和检索 - 矩阵不可变

---
+++ src/数组/NumMatrix.java
@@ -0,0 +1,52 @@
+package 数组;
+
+/*
+304. 二维区域和检索 - 矩阵不可变
+给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2) 。
+示例：
+
+给定 matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+ 
+
+提示：
+
+你可以假设矩阵不可变。
+会多次调用 sumRegion 方法。
+你可以假设 row1 ≤ row2 且 col1 ≤ col2 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/range-sum-query-2d-immutable
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+//通过在初始化时，创建preSum数组来提高后序求和算法的效率
+public class NumMatrix {
+    int[][] preSum;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        if (m > 0) {
+            int n = matrix[0].length;
+            preSum = new int[m + 1][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    preSum[i + 1][j + 1] = preSum[i][j + 1] + preSum[i + 1][j] - preSum[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+
+        return preSum[row2 + 1][col2 + 1] - preSum[row2 + 1][col1] - preSum[row1][col2 + 1] + preSum[row1][col1];
+    }
+}
