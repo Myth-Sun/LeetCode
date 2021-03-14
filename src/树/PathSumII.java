788e585465ab3ea6c24333199bec94ca2333456b
Myth-Sun <1991773694@qq.com>
2021-03-14 16:00:55 +0800

    437. 路径总和 III

---
+++ src/树/PathSumII.java
@@ -0,0 +1,76 @@
+package 树;
+
+/*
+437. 路径总和 III
+给定一个二叉树，它的每个结点都存放着一个整数值。
+
+找出路径和等于给定数值的路径总数。
+
+路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+
+二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+
+示例：
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+返回 3。和等于 8 的路径有:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3.  -3 -> 11
+ */
+public class PathSumII {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int ans = 0;
+
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root, sum);
+        pathSum(root.left, sum);
+        pathSum(root.right, sum);
+        return ans;
+    }
+
+    public void dfs(TreeNode root, int sum) {
+        if (root == null) {
+            return;
+        }
+        if (sum == root.val) {
+            ans++;
+        }
+
+        int value = root.val;
+        dfs(root.left, sum - value);
+        dfs(root.right, sum - value);
+
+    }
+}
