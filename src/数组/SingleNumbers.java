a237f74d1875ed1c4eacad74e4deffd46738e346
Myth-Sun <1991773694@qq.com>
2021-03-29 21:49:35 +0800

    剑指 Offer 56 - I. 数组中数字出现的次数

--- src/数组/SingleNumbers.java
+++ src/数组/SingleNumbers.java
@@ -1,13 +1,45 @@
 package 数组;
 
+/*
+剑指 Offer 56 - I. 数组中数字出现的次数
+一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。
+
+
+
+示例 1：
+
+输入：nums = [4,1,4,6]
+输出：[1,6] 或 [6,1]
+示例 2：
+
+输入：nums = [1,2,10,4,1,4,3,3]
+输出：[2,10] 或 [10,2]
+
+
+限制：
+
+2 <= nums.length <= 10000
+ */
 public class SingleNumbers {
     public int[] singleNumbers(int[] nums) {
-
-        return new int[]{};
+        int[] res = new int[2];
+        int diff = 0;
+        for (int num : nums) {
+            diff ^= num;
+        }
+        diff &= -diff;
+        for (int num : nums) {
+            if ((num & diff) == 0) {
+                res[0] ^= num;
+            } else {
+                res[1] ^= num;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] test = {1, 1, 1,2};
+        int[] test = {1, 1, 1, 2};
         int res = 1;
         for (int num : test) {
             res ^= num;
