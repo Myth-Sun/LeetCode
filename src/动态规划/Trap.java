3e8dc5f3b8e3272b63752aacf5b34d6e3e7da649
Myth-Sun <1991773694@qq.com>
2021-04-02 14:24:28 +0800

    面试题 17.21. 直方图的水量

---
+++ src/动态规划/Trap.java
@@ -0,0 +1,44 @@
+package 动态规划;
+
+/*
+面试题 17.21. 直方图的水量
+给定一个直方图(也称柱状图)，假设有人从上面源源不断地倒水，最后直方图能存多少水量?直方图的宽度为 1。
+
+
+
+上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的直方图，在这种情况下，可以接 6 个单位的水（蓝色部分表示水）。 感谢 Marcos 贡献此图。
+
+示例:
+
+输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+输出: 6
+ */
+public class Trap {
+    public int trap(int[] height) {
+        int ans = 0;
+        int n = height.length;
+        if (n == 0) {
+            return n;
+        }
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+        for (int i = 0; i < n - 1; i++) {
+            ans += (Math.min(leftMax[i], rightMax[i]) - height[i]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Trap trap = new Trap();
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(trap.trap(height));
+    }
+}
