aa0ebee2846df65e091f78bcf93c973c11c11754
Myth-Sun <1991773694@qq.com>
2021-06-27 23:34:56 +0800

    773. 滑动谜题

--- src/并发编程模式/单例模式/SingletonI.java
+++ src/并发编程模式/单例模式/SingletonI.java
@@ -12,7 +12,7 @@ public final class SingletonI implements Serializable {
         return INSTANCE;
     }
 
-    public Object readSolve(){
+    private Object readResolve(){
         return INSTANCE;
     }
 }
